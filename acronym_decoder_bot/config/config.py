a00ba114f424df976efeaa5d9588677d9c65cb6c
Alena Konovalova <konovalova.alena@gmail.com>
2020-11-18 20:48:45 +0300

    fix PATH_TO_DB

--- acronym_decoder_bot/config/config.py
+++ acronym_decoder_bot/config/config.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+
 COLUMN_NAMES = ['full_name', 'acronym', 'reg_number']
 
-PATH_TO_DB = '/Users/alena/Dev/acronym_decoder_bot/db/db_emias.xlsx'
+PATH_TO_DB = Path.joinpath(
+    Path.cwd().parent,
+    'db/db_emias.xlsx'
+    )
 
 MIN_MATCHES_TO_SHOW = 8
